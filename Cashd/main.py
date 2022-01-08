b82cf7f3794467940546667d333f2426ef1e4aea
Vitor Ferreira Lins <vflins@live.com>
2022-01-08 12:25:13 -0300

    Update main.py
    - give up firebird integration
    - trying to store data in text files with numpy

--- Cashd/main.py
+++ Cashd/main.py
@@ -1,19 +1,38 @@
 # import PySimpleGUI as sgui
 import datetime as dt
-import fbd
-
-if isfile(/database/db.fdb)
+import os
+import numpy as np
 
 def date_handle(str_date):
+    """Converts the ddmmaaaa string input to actual date format"""
     tru_date = dt.date(
-        int( str_date[4:] ), 
-        int( str_date[2:4] ), 
-        int( str_date[0:2] )
+        year = int( str_date[4:] ), 
+        month = int( str_date[2:4] ), 
+        day = int( str_date[0:2] )
     )
     return tru_date
 
+def time_handle(str_time):
+    """Converts the hhmmss string input to actual time format"""
+    tru_time = dt.time(
+        second = int( str_time[4:] ),
+        minute = int( str_time[2:4] ),
+        hour = int( str_time[0:2] )
+    )
+    return tru_time
+
+def data_storage(array):
+    """Save the array of inputs in a text file"""
+    db_dir = "/database/cf.npy"
+    if not path.isdir(db_dir):
+        path.createdir(db_dir)
+    np.savetxt(db_dir, array, delimiter=",", newline=":")
+
 def prompter():
-    inp_date = date_handle( input("Insira a data (ddmmaaaa): \n") )
-    inp_value = input("Insira o valor (0000.00): \n")
+    inp_date = date_handle( input("Insira a data (ddmmaaaa):\n") )
+    inp_time = time_handle( input("Insira o horário (hhmmss):\n"))
+    inp_value = float( input("Insira o valor (0000.00):\n") )
+
+    data_storage([inp_date, inp_time, inp_value])
 
 prompter()
