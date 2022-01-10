6b8e3dc978602a15fc1feb119ccf746c27d5eb9d
Vitor Ferreira Lins <vflins@live.com>
2022-01-10 10:21:05 -0300

    Update main.py
    - inputs handled separately;
    - countdown to close after error changed 3secs to 5secs

--- Cashd/main.py
+++ Cashd/main.py
@@ -3,35 +3,42 @@ import os
 import time
 import datetime as dt
 
-def date_handle(str_date):
-    """Converts the ddmmaaaa string input to actual date format"""
-    try:
-        tru_date = dt.date(
-            year = int( str_date[4:] ), 
-            month = int( str_date[2:4] ), 
-            day = int( str_date[0:2] )
-        )
-        return tru_date
-    except:
-        print("Data inválida!\n\n")
-        prompter()
-
-def time_handle(str_time):
-    """Converts the hhmmss string input to actual time format"""
-    try:
-        tru_time = dt.time(
-            second = int( str_time[4:] ),
-            minute = int( str_time[2:4] ),
-            hour = int( str_time[0:2] )
-        )
-        return tru_time
-    except:
-        print("Horário inválido!\n\n")
-        prompter()
+def inp_handle(type_to = ["d", "t", "n"]):
+    if type_to == "d":
+        try:
+            str_input = input("Insira a data (ddmmaaaa):\n")
+            tru_date = dt.date(
+                year = int( str_input[4:] ), 
+                month = int( str_input[2:4] ), 
+                day = int( str_input[0:2] )
+            )
+            return tru_date
+        except:
+            print("Data inválida!\n\n")
+            prompter()
+    elif type_to == "t":
+        try:
+            str_input = input("Insira o horário (hhmmss):\n")
+            tru_time = dt.time(
+                second = int( str_input[4:] ),
+                minute = int( str_input[2:4] ),
+                hour = int( str_input[0:2] )
+            )
+            return tru_time
+        except:
+            print("Horário inválido!\n\n")
+            prompter()
+    elif type_to == "n":
+        try:
+            str_input = input("Insira o valor (0000.00):\n")
+            return float( str_input )
+        except:
+            print("Valor inválido!\n\n")
+            prompter()
 
 def data_storage(array):
     """Save the array of inputs in a text file"""
-    work_dir = "C:\\Users\\vflin\OneDrive\\Documentos\\GitHub\\Projects\\Cashd"
+    work_dir = os.getcwd() + "\\Cashd"
     os.chdir(work_dir)
 
     regfile = open("input_reg.csv", "a")
@@ -46,9 +53,9 @@ def countdown_message(message, seconds = 10):
         seconds -= 1
 
 def prompter():
-    inp_date = date_handle( input("Insira a data (ddmmaaaa):\n") )
-    inp_time = time_handle( input("Insira o horário (hhmmss):\n"))
-    inp_value = float( input("Insira o valor (0000.00):\n") )
+    inp_date = inp_handle( type_to = "d" )
+    inp_time = inp_handle( type_to = "t" )
+    inp_value = inp_handle( type_to = "n" )
     try:
         data_storage(
             [str(inp_date)+",", str(inp_time)+",", str(inp_value), "\n"]
@@ -56,7 +63,7 @@ def prompter():
         print("Dados armazenados!\n")
     except Exception as prompter_error:
         print(prompter_error)
-        countdown_message("Falha no aramazenamento dos dados, fechando em", 3)
+        countdown_message("Falha no aramazenamento dos dados, fechando em", 5)
     finally:
         prompter()
 
