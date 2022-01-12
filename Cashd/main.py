0b322b213b5192713a183e76301c07e73e3dfcb4
Vitor Ferreira Lins <vflins@live.com>
2022-01-12 11:54:22 -0300

    Separating input handlers

--- Cashd/main.py
+++ Cashd/main.py
@@ -10,52 +10,51 @@ def shortcut_handle(shortcut):
    elif shortcut.upper() == "AGORA":
       return dt.datetime.now().strftime("%H:%M:%S")
 
-def inp_handle(type_to = ["d", "t", "n"]):
-   if type_to == "d":
-      try:
-         str_input = input("Insira a data (ddmmaaaa):\n")
-         tru_date = dt.date(
-            year = int( str_input[4:] ), 
-            month = int( str_input[2:4] ), 
-            day = int( str_input[0:2] )
-         )
+def inp_date_handle():
+   try:
+      str_input = input("Insira a data (ddmmaaaa):\n")
+      tru_date = str(dt.date(
+         year = int( str_input[4:] ), 
+         month = int( str_input[2:4] ), 
+         day = int( str_input[0:2] )
+      ))
+      return tru_date
+   except:
+      str_input = input("Insira a data (ddmmaaaa):\n")
+      tru_date = shortcut_handle( str_input )
+      if bool(dt.datetime.strptime(tru_date, "%Y-%m-%d")):
          return tru_date
-      except ValueError:
-         str_input = input("Insira a data (ddmmaaaa):\n")
-         tru_date = shortcut_handle( str_input )
-         if bool(dt.datetime.strptime(tru_time, "%Y-%m-%d")):
-            return tru_date
-         else:
-            raise Exception("Valor icorreto para este campo!")
-      except:
+      else:
          print("Data inválida!\n\n")
          prompter()
-   elif type_to == "t":
-      try:
-         str_input = input("Insira o horário (hhmmss):\n")
-         tru_time = dt.time(
-            second = int( str_input[4:] ),
-            minute = int( str_input[2:4] ),
-            hour = int( str_input[0:2] )
-         )
+
+def inp_time_handle():
+   try:
+      str_input = input("Insira o horário (hhmmss):\n")
+      tru_time = dt.time(
+         second = int( str_input[4:] ),
+         minute = int( str_input[2:4] ),
+         hour = int( str_input[0:2] )
+      )
+      return tru_time
+   except ValueError:
+      str_input = input("Insira o horário (hhmmss):\n")
+      tru_time = shortcut_handle( str_input )
+      if bool(dt.datetime.strptime(tru_time, "%H:%M:%S")):
          return tru_time
-      except ValueError:
-         str_input = input("Insira o horário (hhmmss):\n")
-         tru_time = shortcut_handle( str_input )
-         if bool(dt.datetime.strptime(tru_time, "%H:%M:%S")):
-            return tru_time
-         else:
-            raise Exception("Valor icorreto para este campo!")
-      except:
-         print("Horário inválido!\n\n")
-         prompter()
-   elif type_to == "n":
-      try:
-         str_input = input("Insira o valor (0000.00):\n")
-         return float( str_input )
-      except:
-         print("Valor inválido!\n\n")
-         prompter()
+      else:
+         raise Exception("Valor icorreto para este campo!")
+   except:
+      print("Horário inválido!\n\n")
+      prompter()
+
+def inp_numeric_handle():
+   try:
+      str_input = input("Insira o valor (0000.00):\n")
+      return float( str_input )
+   except:
+      print("Valor inválido!\n\n")
+      prompter()
 
 def manage_dir():
    global work_dir
@@ -92,9 +91,9 @@ def countdown_message(message, seconds = 5):
       seconds -= 1
 
 def prompter():
-   inp_date = inp_handle( type_to = "d" )
-   inp_time = inp_handle( type_to = "t" )
-   inp_value = inp_handle( type_to = "n" )
+   inp_date = inp_date_handle()
+   inp_time = inp_time_handle()
+   inp_value = inp_numeric_handle()
    try:
       data_storage(
          [str(inp_date)+",", str(inp_time)+",", str(inp_value), "\n"]
