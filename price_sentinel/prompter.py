97bcadb10bc53a15353e8cc3d0708c283ae8b4cf
Vitor Ferreira Lins <vflins@live.com>
2022-01-22 18:17:33 -0300

    Update prompter.py

--- price_sentinel/prompter.py
+++ price_sentinel/prompter.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 STORE_LIST = {
   "1" : "Amazon",
   "2" : "Dell"
@@ -12,4 +13,25 @@ for c, s in STORE_LIST.items():
 STORE_SELEC = input('\n\n',"Enter the store's code: ")
 
 return STORE_SELEC
+=======
+from cProfile import label
+
+
+AVAILABLE_STORES = {
+    '1' : 'Amazon',
+    '2' : 'Dell'
+}
+
+print('Current available stores:')
+print("{:<8} {:<15}".format('CODE','STORE'))
+for k, s in AVAILABLE_STORES.items():
+   label = s
+   print("{:<8} {:<15}".format(k, label))
+print()
+
+PRODUCT_STORE = input('Enter the store code: ')
+#PRODUCT_URL = input('Paste the URL to the page of the product: ')
+
+print('Connecting to', AVAILABLE_STORES[PRODUCT_STORE], '...')
+>>>>>>> Stashed changes
 
