0111b35cbcceccb9a40b0b6045225e540b310369
Vitor Ferreira Lins <vflins@live.com>
2022-02-05 08:04:33 -0300

    Update main.py

--- Cashd/main.py
+++ Cashd/main.py
@@ -3,7 +3,7 @@ import msg_handler as mh
 import db_manager as dm
 
 print("Bem vindo ao Cashd, seus dados serão salvos em:", dm.WORK_DIR)
-print("Atalhos disponíveis:")
+print("\nAtalhos disponíveis:")
 for a, b in zip(*mh.SHORTCUTS):
    print(a.ljust(10), b)
 print("")
