738edf0a212f130784beb145a9dd57abaf576198
Vitor Ferreira Lins <42384822+VFLins@users.noreply.github.com>
2025-01-09 16:48:42 -0300

    Add files via upload

--- labor_monitor/src_sidram.R
+++ labor_monitor/src_sidram.R
@@ -1,9 +1,14 @@
 ##### Coleta de dados mensais a partir dos servidores oficiais SIDRA
 ## recomendado manter codificação do documento em 'latin1'
+library(zoo)
+library(lubridate)
+library(dplyr)
+library(sidrar)
+
 meses <- seq(
-  from =as.yearmon({Sys.Date() %>% year() - 6}),
+  from =as.yearmon({lubridate::year(Sys.Date()) - 6}),
   to =as.yearmon(Sys.Date()),
-  by =1/12) %>%
+  by =1/12) |>
   as.Date()
 
 # dados do sidra
@@ -14,29 +19,29 @@ sidra <- list()
 sidra$unemp <- get_sidra(
   6381,
   variable =4099,
-  period ={meses %>% format("%Y%m")}
-) %>% select(`Trimestre Móvel (Código)`, Valor) %>%
-  setNames(c("meses", "unemp")) %>%
-  mutate(meses =as.yearmon(meses, format ="%Y%m"))
+  period ={meses |> format("%Y%m")}
+) |> select(`Trimestre Móvel (Código)`, Valor) |>
+  setNames(c("meses", "unemp")) |>
+  mutate(meses = as.yearmon(meses, format ="%Y%m"))
 
 # Inflacao INPC mensal
 sidra$prec0 <- get_sidra(
   1736,
   variable =44,
-  period ={meses %>% format("%Y%m")}
-) %>% select(`Mês (Código)`, Valor) %>%
-  setNames(c("meses", "prec0")) %>%
+  period ={meses |> format("%Y%m")}
+) |> select(`Mês (Código)`, Valor) |>
+  setNames(c("meses", "prec0")) |>
   mutate(meses =as.yearmon(meses, format ="%Y%m"))
 
 # Inflacao IPCA acumulado 12 meses
 sidra$infla <- get_sidra(
   1737,
   variable =c(2265,2266),
-  period ={meses %>% format("%Y%m")}
-) %>% select(`Mês (Código)`, Variável, Valor) %>%
-  setNames(c("meses", "grupo", "preco")) %>%
-  mutate(meses =as.yearmon(meses, format ="%Y%m")) %>%
-  pivot_wider(names_from =grupo, values_from =preco)
+  period ={meses |> format("%Y%m")}
+) |> select(`Mês (Código)`, Variável, Valor) |>
+  setNames(c("meses", "grupo", "preco")) |>
+  mutate(meses =as.yearmon(meses, format ="%Y%m")) |>
+  tidyr::pivot_wider(names_from =grupo, values_from =preco)
 dicio_infla <- names(sidra$infla[1, 2:ncol(sidra$infla)])
 names(sidra$infla) <- c("meses", "inf12", "inf00")
 names(dicio_infla) <- c("inf12", "inf00")
@@ -44,11 +49,14 @@ names(dicio_infla) <- c("inf12", "inf00")
 # Inflacao INPC mensal por grupo
 sidra$preco <- get_sidra(
   api ="/t/7063/n1/all/v/44/p/all/c315/7170,7445,7486,7558,7625,7660,7712,7766,7786/d/v44%202"
-) %>% select(`Mês (Código)`, `Geral, grupo, subgrupo, item e subitem`, Valor) %>%
-  setNames(c("meses", "grupo", "preco")) %>%
-  mutate(meses =as.yearmon(meses, format ="%Y%m")) %>%
-  .[.$meses %in% as.yearmon(meses), ] %>%
-  pivot_wider(names_from =grupo, values_from =preco)
+) |>
+  select(`Mês (Código)`, `Geral, grupo, subgrupo, item e subitem`, Valor) |>
+  setNames(c("meses", "grupo", "preco")) |>
+  mutate(meses =as.yearmon(meses, format ="%Y%m"))
+
+sidra$preco <- subset(sidra$preco, sidra$preco$meses %in% as.yearmon(meses)) |>
+  tidyr::pivot_wider(names_from =grupo, values_from =preco)
+
 dicio_preco <- names(sidra$preco[1, 2:ncol(sidra$preco)])
 names(sidra$preco) <- c("meses", paste0("prec", 1:9))
 names(dicio_preco) <- paste0("prec", 1:9)
--- labor_monitor/src_sidrat.R
+++ labor_monitor/src_sidrat.R
@@ -1,61 +1,60 @@
 ##### Coleta de dados trimestrais a partir dos servidores oficiais SIDRA
 ## recomendado manter codificação do documento em 'latin1'
+library(sidrar)
+library(dplyr)
+
 trimestres <- seq(
-  from =as.yearqtr({Sys.Date() %>% year() - 6}),
-  to =as.yearqtr(Sys.Date()),
-  by =1/4) %>%
-  gsub(" Q", "0", .)
+  from = zoo::as.yearqtr({lubridate::year(Sys.Date()) - 6}),
+  to = zoo::as.yearqtr(Sys.Date()),
+  by =1/4)
+trimestres <- gsub(" Q", "0", trimestres)
 
 sidra <- list()
 #sidra$trimestre <- trimestres
 
 # Valor adicionado total, agro, industria, e serviços, respectivamente
-sidra$vAdic <- get_sidra(
-  1846,
-  period =trimestres) %>%
-  select(c("Trimestre (Código)", "Setores e subsetores (Código)", "Valor")) %>%
-    setNames(c("trimestre", "setores", "valor")) %>%
-    .[.$setores %in% c(90705, 90687, 90691, 90696), ] %>%
-    mutate(setores =paste0("v", setores), trimestre =as.yearqtr(trimestre, format ="%Y%q")) %>%
-    pivot_wider(names_from =setores, values_from =valor) %>%
-    select(c(trimestre, v90705, v90687, v90691, v90696)) %>%
-    setNames(c("trimestre", "vAdic_total", "vAdic_agro", "vAdic_indu", "vAdic_serv"))
+sidra$vAdic <- get_sidra(1846, period = trimestres) |>
+  select(c("Trimestre (Código)", "Setores e subsetores (Código)", "Valor")) |>
+  setNames(c("trimestre", "setores", "valor"))
+
+sidra$vAdic <- sidra$vAdic[sidra$vAdic$setores %in% c(90705, 90687, 90691, 90696), ] |>
+  mutate(setores =paste0("v", setores), trimestre = zoo::as.yearqtr(trimestre, format ="%Y%q")) |>
+  tidyr::pivot_wider(names_from =setores, values_from =valor) |>
+  select(c(trimestre, v90705, v90687, v90691, v90696)) |>
+  setNames(c("trimestre", "vAdic_total", "vAdic_agro", "vAdic_indu", "vAdic_serv"))
 
 # Valor adicionado a preços de 1995 total, agro, industria, e serviços, respectivamente
-sidra$vAdi2 <- get_sidra(
-  6612,
-  period =trimestres
-) %>%
-  select(c("Trimestre (Código)", "Setores e subsetores (Código)", "Valor")) %>%
-  setNames(c("trimestre", "setores", "valor")) %>%
-  .[.$setores %in% c(90705, 90687, 90691, 90696), ] %>%
-  mutate(setores =paste0("v", setores), trimestre =as.yearqtr(trimestre, format ="%Y%q")) %>%
-  pivot_wider(names_from =setores, values_from =valor) %>%
-  select(c(trimestre, v90705, v90687, v90691, v90696)) %>%
+sidra$vAdi2 <- get_sidra(6612, period =trimestres) |>
+  select(c("Trimestre (Código)", "Setores e subsetores (Código)", "Valor")) |>
+  setNames(c("trimestre", "setores", "valor"))
+
+sidra$vAdi2 <- sidra$vAdi2[sidra$vAdi2$setores %in% c(90705, 90687, 90691, 90696), ] |>
+  mutate(setores =paste0("v", setores), trimestre = zoo::as.yearqtr(trimestre, format ="%Y%q")) |>
+  tidyr::pivot_wider(names_from =setores, values_from =valor) |>
+  select(c(trimestre, v90705, v90687, v90691, v90696)) |>
   setNames(c("trimestre", "vAdi2_total", "vAdi2_agro", "vAdi2_indu", "vAdi2_serv"))
 
 # População empregada
-sidra$popEm <- get_sidra(
-  4092,
-  period =trimestres,
-  variable =1641) %>%
+sidra$popEm <- get_sidra(4092, period =trimestres, variable =1641) |>
   select(
     c("Trimestre (Código)",
     "Condição em relação à força de trabalho e condição de ocupação (Código)",
     "Valor")
-  ) %>%
-  setNames(c("trimestre", "setores", "popEm")) %>%
-  subset(setores ==32387) %>%
-  select(trimestre, popEm) %>%
-  mutate(trimestre =as.yearqtr(trimestre, format ="%Y%q"))
+  ) |>
+  setNames(c("trimestre", "setores", "popEm")) |>
+  subset(setores == 32387) |>
+  select(trimestre, popEm) |>
+  mutate(trimestre = zoo::as.yearqtr(trimestre, format = "%Y%q"))
 
-sidra$reMed <-  get_sidra(5431, variable =5931, period =trimestres) %>% 
-  select(c("Trimestre (Código)", "Nível de instrução", "Nível de instrução (Código)", "Valor")) %>%
-  setNames(c("trimestre", "inst_nm", "inst_cd", "reMed")) %>%
-  .[.$inst_cd %in% c("120704", "120706", "11628", "11630", "11632"), ] %>%
-  select(-inst_cd) %>%
-  pivot_wider(names_from =inst_nm, values_from =reMed, id_cols =trimestre) %>%
-  mutate(trimestre =as.yearqtr(trimestre, format ="%Y%q"))
+sidra$reMed <-  get_sidra(5438, variable =5932, period =trimestres) |> 
+  select(c("Trimestre (Código)", "Nível de instrução", "Nível de instrução (Código)", "Valor")) |>
+  setNames(c("trimestre", "inst_nm", "inst_cd", "reMed"))
+
+sidra$reMed <- sidra$reMed[sidra$reMed$inst_cd %in% c("120704", "120706", "11628", "11630", "11632"), ] |>
+  select(-inst_cd) |>
+  tidyr::pivot_wider(names_from =inst_nm, values_from =reMed, id_cols =trimestre) |>
+  mutate(trimestre = zoo::as.yearqtr(trimestre, format ="%Y%q"))
+
 dicio_reMed <- names(sidra$reMed[1, 2:ncol(sidra$reMed)])
 names(sidra$reMed) <- c("trimestre", paste0("reMe", 0:4))
 names(dicio_reMed) <- paste0("reMe", 0:4)
